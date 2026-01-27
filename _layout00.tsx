cd7ec034ad194f9663d67da88eaa36c905014ac6
fotomain <foto888999@gmail.com>
2026-01-27 13:55:46 +0200

    indexTESTS

--- _layout00.tsx
+++ _layout00.tsx
@@ -1,4 +1,5 @@
 // import { Stack } from "expo-router";
+
 import * as SplashScreen from "expo-splash-screen";
 import {StatusBar} from "expo-status-bar";
 // import { StatusBar } from "expo-status-bar";
