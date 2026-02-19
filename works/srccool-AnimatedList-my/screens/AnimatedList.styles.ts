395b27b0f2637f2d38393a6e37517800021491d5
fotomain <foto888999@gmail.com>
2026-02-19 19:11:17 +0200

    AppListFinal1File-BEST

--- works/srccool-AnimatedList-my/screens/AnimatedList.styles.ts
+++
@@ -1,6 +0,0 @@
-import {StyleSheet} from 'react-native';
-import {Color_Pallete} from '../constants';
-
-export const styles = StyleSheet.create({
-  listContainer: {backgroundColor: Color_Pallete.metal_black, height: '100%'},
-});
